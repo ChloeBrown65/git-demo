8063d3a32a0e390eddcfce65cf9b19e92444e702
Chloe.Brown65 <chloe.brown65@thehutgroup.com>
2022-09-27 13:58:06 +0100

    Add mars feature

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
+import sys
 
 def hello():
-	print("hello world")
-
-if name == 'main':
+	if sys.argv[1] == "Mars":
+		hellomars()
+	else: 
+		helloworld()
+def hellomars():
+	print("Hello mars")
+def helloworld():
+	print("Hello world")
+if __name__ == '__main__':
 	hello()
+
